9bff1237baedf09e5a3e40c23f00a1ac3927511e
lizhao007 <36907006+lizhao007@users.noreply.github.com>
2023-11-11 16:09:53 +0800

    Update DenovoAS_Finder.py readme

--- DenovoAS_Finder.py
+++ DenovoAS_Finder.py
@@ -3,7 +3,7 @@
 Created on Thu Apr 18 10:26:38 2019
 Changed on Mon Oct 19 14:53:58 2020
 @author: Li,Zhao
-Usage:python AS_Finder_Denovo.py
+Usage:python DenovoAS_Finder.py test.txt data.fa
 Note:First, BLAST+ and mcl should be loaded, and test.txt and data.fa should be prepared. The test.txt is the result of test.fa alignment by BLAST+, which inculde 12 lines "gene label pident length mismatch gapopen qstart qend sstart send evalue bitscore". "gene" line combines isoform1 and isoform2 with *, and "label" line is consist of 0 or 1, while 0 presents do not come from the same gene and 1 presents yes. Other lines produced by "blastn -query test.fa -db test -outfmt 6 -out test.txt -evalue 1e-10". The data.fa is the isoform file which you want to classify.
 """
 import warnings
